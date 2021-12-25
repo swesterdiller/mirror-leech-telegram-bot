7968bdb4aaec43b43d740dd10462787f97740fd3
swesterdiller <96383201+swesterdiller@users.noreply.github.com>
2021-12-25 22:55:37 +0100

    Update gdriveTools.py

--- bot/helper/mirror_utils/upload_utils/gdriveTools.py
+++ bot/helper/mirror_utils/upload_utils/gdriveTools.py
@@ -452,7 +452,7 @@ class GoogleDriveHelper:
     def create_directory(self, directory_name, parent_id):
         file_metadata = {
             "name": directory_name,
-            "description": "Google Drive",
+            "description": "♻️Google Drive♻️",
             "mimeType": self.__G_DRIVE_DIR_MIME_TYPE
         }
         if parent_id is not None:
